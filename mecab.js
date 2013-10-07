431509dfbe114c8cef3366c20064127b15dddfd7
Bryant Teja <beeant@gmail.com>
2013-10-08 01:41:19 +0900

    add parseFormat, format result into object with explanation keys

--- mecab.js
+++ mecab.js
@@ -1,22 +1,30 @@
-var exec     = require('child_process').exec;
+var exec = require('child_process').exec;
+var async = require('async');
 
 // 後方互換のため
 var MeCab = function() {};
 MeCab.prototype = {
-	_parseMeCabResult : function(result) {
-		var ret = [];
-		result.split('\n').forEach(function(line) {
-			ret.push(line.replace('\t', ',').split(','));
-		});
-		return ret;
-	},
-	parse : function(str, callback) {
-		exec('echo ' + str + ' | mecab', function(err, result) {
-			if (err) {
-				callback(err, null);
-				return;
-			}
-			callback(null, MeCab._parseMeCabResult(result).slice(0,-2));
+	_format: function(parsedResult, callback) {
+		var result = [];
+		// Reference: http://mecab.googlecode.com/svn/trunk/mecab/doc/index.html
+		// 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+		async.each(parsedResult, function(parsed, callback) {
+			if(parsed.length <= 8) { return callback(); }
+			result.push({
+				kanji: parsed[0],
+				lexical: parsed[1],
+				compound: parsed[2],
+				compound2: parsed[3],
+				compound3: parsed[4],
+				conjugation: parsed[5],
+				inflection: parsed[6],
+				original: parsed[7],
+				reading: parsed[8],
+				pronunciation: parsed[9] || ''
+			});
+			callback();
+		}, function(err) {
+			callback(err, result);
 		});
 	},
 	_wakatsu : function(arr) {
@@ -25,6 +33,27 @@ MeCab.prototype = {
 			ret.push(arr[i][0]);
 		}
 		return ret;
+	},
+	_parseMeCabResult : function(result) {
+		var ret = [];
+		result.split('\n').forEach(function(line) {
+			ret.push(line.replace('\t', ',').split(','));
+		});
+		return ret;
+	},
+	parse : function(str, callback) {
+		exec('echo "' + str + '" | mecab', function(err, result) {
+			if (err) {
+				callback(err, null);
+				return;
+			}
+			callback(null, MeCab._parseMeCabResult(result).slice(0,-2));
+		});
+	},
+	parseFormat: function(str, callback) {
+		MeCab.parse(str, function(err, result) {
+			MeCab._format(result, callback);
+		});
 	},
 	wakachi : function(str, callback) {
 		MeCab.parse(str, function(err, arr) {
